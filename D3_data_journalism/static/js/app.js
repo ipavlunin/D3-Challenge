424bc60c231d130cf0f98ae7abf6dce8c200c56b
ipavlunin <ipavlunin@gmail.com>
2020-11-24 18:03:43 -0600

    SVG wrapper

--- D3_data_journalism/static/js/app.js
+++ D3_data_journalism/static/js/app.js
@@ -1,1 +1,28 @@
-// @TODO: YOUR CODE HERE!
+// Application to render data into interactive D3 plots.
+
+// Creating a pattern area to render future charts
+
+var svgWidth = 800;
+var svgHeight = 500;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// SVG wrapper, append an SVG group that will hold our chart
+
+var svg = d3
+    .select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
